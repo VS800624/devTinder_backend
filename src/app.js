6d9e038ca7673959bb7184f52e934acee9b27482
VS800624 <vishalsingh800624@gmail.com>
2025-10-28 17:06:27 +0530

    wild-card error handling

--- src/app.js
+++ src/app.js
@@ -1,36 +1,32 @@
 const express = require("express");
-
+const {authAdmin, authUser} = require("./middlewares/auth")
 const app = express();
 
-// app.use("/router", [rH1, rH2, rH3, rH4, rH5])
-// or  app.use("/router", [rH1, rH2], rH3, rH4, rH5)
-// rH means route handler
-
-app.use(
-  "/user",
-  (req, res, next) => {
-    // route handler 1
-    // res.send("response 1")
-    console.log("Handling the route user 1!");
-    next();   // moves to next middleware
-  },
-  (req, res, next) => {
-    // route handler 2
-    // res.send("2nd response");
-    console.log("Handling the route user 2!");
-    next()
-  },
-  (req, res,next) => {
-    // route handler 3
-    res.send("3nd response");
-    console.log("Handling the route user 3!");
-  },
-  (req, res) => {
-    // route handler 4
-    res.send("4th response");
-    console.log("Handling the route user 4!");
+app.use("/" , (err, req ,res, next) => {
+   if(err) {
+    // log your error 
+    res.status(500).send("something went wrong")
   }
-);
+})
+
+app.get("/user",  (req,res) => {
+  try {
+
+  } catch (err) {
+    res.status(500).send("some error occurred please contact support team")
+  }
+// logic of DB call and get user data 
+  throw new error("something went wrong")
+  res.send("User logged in successfully")
+}) 
+
+app.use("/" , (err, req ,res, next) => {
+   if(err) {
+    // log your error 
+    res.status(500).send("something went wrong")
+  }
+})
+
 app.listen(3000, () => {
   console.log("Server  is successfully listening on port 3000...");
 });
