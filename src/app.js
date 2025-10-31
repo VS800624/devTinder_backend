d53ab9986e4679f7bb07f9cddb39ce33d319c067
VS800624 <vishalsingh800624@gmail.com>
2025-10-31 16:06:33 +0530

    make some get APIs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {authAdmin, authUser} = require("./middlewares/auth")
 const app = express();
+const User = require("./models/user")
 
 const connectDB = require("./config/database")
 
@@ -17,6 +18,39 @@ app.post("/signup", async(req,res) => {
     res.send("User added successfully!")
   } catch (err) {
     res.status(400).send("Error adding the user:" + err.message)
+  }
+})
+
+app.get("/user", async (req,res) => {
+  
+  const userEmail = req.body.emailId
+
+  try {
+    const user = await User.findOne({emailId: userEmail})
+    if(!user) {
+      res.status(404).send("User not found")
+    } else {
+      res.send(user)
+    }
+
+    // const users = await User.find({ emailId: userEmail })
+    // if(!users){
+    //   res.send(400).send("User not found")
+    // }else {
+    //   res.send(users)
+    // }
+  } catch(err) {
+    res.status(400).send("Something went wrong")
+  }
+})
+
+// Feed API - Get/feed - get all the users from the database
+app.get("/feed", async (req,res) => {
+  try {
+    const users = await User.find({})
+    res.send(users)
+  } catch (err) {
+    res.status(400).send("Something went wrong")
   }
 })
 
