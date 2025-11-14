96d8041b55a93bbf0f6f8ffd9414e007eab5d0b2
VS800624 <vishalsingh800624@gmail.com>
2025-11-14 17:31:05 +0530

    created connectionRequest model

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const User = require("../models/user");
 const requestRouter = express.Router()
 
-requestRouter.get("/sendConnectionRequest", userAuth, (req, res) => {
-  const user = req.user;
 
-  // sending a connection request
-  console.log("Sending a connection request");
-  res.send(user.firstName + " sent the connection request!");
-});
 
 module.exports = requestRouter
