3a44275bf38a7219e4e0fb50d033c164ccdc6bcc
VS800624 <vishalsingh800624@gmail.com>
2025-11-14 17:39:27 +0530

    created request/send/:status/:toUserId API

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const User = require("../models/user");
+const ConnectionRequest = require("../models/connectionRequest");
 const requestRouter = express.Router()
 
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async(req, res) => {
+  try {
+    const fromUserId = req.user._id  // the logged in user sending the connection request
+    const toUserId = req.params.toUserId
+    const status = req.params.status
 
+    const allowedStatus = ["ignored", "interested"]
+    if(!allowedStatus.includes(status)){
+     return res.status(400).json({message: "Invalid status type: " + status})
+    }
+
+    const toUser = await User.findById(toUserId)
+    if(!toUserId){
+      return res.status(400).json({message: "User not found"})
+    }
+
+    // new instance of ConnectionRequest model
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status
+    })
+
+    const data = await connectionRequest.save()
+      res.json({
+        message: (status === "interested"? req.user.firstName + " is " + status + " in " + toUser.firstName: req.user.firstName + " has " + status  + " " + toUser.firstName),
+        // data: data //or 
+        data
+      })
+  } catch (err){
+    res.status(400).send("ERROR:" + err.message)
+  }
+
+});
 
 module.exports = requestRouter
