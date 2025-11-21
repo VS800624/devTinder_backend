d99465f31599ac3e54c34818d0f7ef52e2bc8cae
VS800624 <vishalsingh800624@gmail.com>
2025-11-21 12:53:15 +0530

    adding origin and credentails in cors

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -57,7 +57,7 @@ authRouter.post("/login", async (req,res) => {
         expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
         httpOnly: true,
       });
-      res.send("Logged in successfully!!!");
+      res.json({message: "Logged in successfully!!!", data: user});
     }
   } catch(err) {
   }
