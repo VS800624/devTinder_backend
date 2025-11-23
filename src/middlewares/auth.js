b53d38bc514af8704036d355a3fc23b62d421b40
VS800624 <vishalsingh800624@gmail.com>
2025-11-23 11:06:58 +0530

    created logout

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,11 +8,11 @@ const userAuth = async(req, res, next) => {
     //   const {token} = cookies // or
     const { token } = req.cookies;
     if (!token) {
-      res.status(401).send("Please login!")
+      return res.status(401).send("Please Login!")
     }
       // validate the token (secret key) and decode and return the payload (the data (_id) you originally stored in the token)
     const decodedMessage = await jwt.verify(token, "DEV@TINDER$619")
-    const {_id} = decodedMessage
+    const {_id} = decodedMessage 
 
     const user = await User.findById(_id)
     if (!user){
