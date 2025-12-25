3cb5d3586557f6d870336d4b5601026e2ede7112
VS800624 <vishalsingh800624@gmail.com>
2025-12-25 16:38:21 +0530

    Add payment module

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,25 +9,31 @@ const userAuth = async(req, res, next) => {
     // if (req.method === "OPTIONS") {
     //   return next();
     // }
+    // console.log("STEP 1: Cookies OK");
     
     //   const cookies = req.cookies
     //   const {token} = cookies // or
     const { token } = req.cookies;
+      //  console.log("STEP 2: Token =", token);
     if (!token) {
       return res.status(401).send("Please Login!")
     }
       // validate the token (secret key) and decode and return the payload (the data (_id) you originally stored in the token)
     const decodedMessage = await jwt.verify(token, process.env.JWT_SECRET)
+    //  console.log("STEP 3: Decoded =", decodedMessage);
     const {_id} = decodedMessage 
 
     const user = await User.findById(_id)
+    // console.log("STEP 4: User =", user);
     if (!user){
       throw new Error("User not found")
     }
+    // console.log("COOKIES:", req.cookies);
     req.user = user
     next()
     
   } catch (err) {
+    //  console.error("AUTH ERROR:", err.message);
     res.status(400).send("ERROR:" + err.message)
   }
 };
--- src/routes/payment.js
+++ src/routes/payment.js
@@ -2,10 +2,10 @@ const express = require("express");
 const paymentRouter = express.Router();
 const razorPayInstance = require("../utils/razorpay");
 const { userAuth } = require("../middlewares/auth");
-
+const Payment = require("../models/payment")
 paymentRouter.post("/payment/create", userAuth, async (req, res) => {
   try {
-    // this is the code to create order on razorPay
+    // this is the code to create order on razorPay , we have initialized our razorpay instance with key 
     const order = await razorPayInstance.orders.create({
       amount: 50000, //this value is in lower denomination of currency(this is in paisa)
       currency: "INR",
@@ -17,11 +17,22 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
       },
     }); //this will return a promise and we will create an order
 
+    // console.log(order);
     // Save it in my database
-    console.log(order);
+    const payment = new Payment({
+      userId: req.user._id,       //the userId will come from userAuth
+      orderId: order.id,
+      status: order.status,
+      amount: order.amount,
+      currency: order.currency,
+      receipt: order.receipt,
+      notes: order.notes
+    })
+
+    const savedPayment = await payment.save()
 
     // return back my order details to frontend
-    res.json({ data: order });
+    res.json({ ...savedPayment.toJSON() });
   } catch (err) {
     return res.status(500).json({ success: false, message: err.message });
   }
